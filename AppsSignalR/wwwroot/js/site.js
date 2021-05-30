e3f2f1e06d5c9451f5a280f1b745fee938ad6497
ettarak <ettarak.zouhair@gmail.com>
2021-05-30 13:34:23 +0200

    Coding, Create Class ChatHub and call them in startup

---
+++ AppsSignalR/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+// Connection 
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ":  " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSnend", "click", function () {
+    var fromUser = $("#txtUser").val();
+    var messsage = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", fromUser, messsage);
+});
